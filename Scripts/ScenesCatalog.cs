188b1121c2c7cf9bd71c205ec8903b3e7156c79c
HARHAL-DEV_ICE\harhal <harhal19@gmail.com>
2024-01-28 15:58:28 +0100

    Add Icon.png back

--- Scripts/ScenesCatalog.cs
+++ Scripts/ScenesCatalog.cs
@@ -7,17 +7,18 @@ public class ScenesCatalog : Node
     [Export] private Array<PackedScene> _scenes;
     private static Array<PackedScene> StaticCatalog;
     [Export] private NodePath _musicPath;
-    public static Node Music;
 
     public override void _Ready()
     {
         base._Ready();
         StaticCatalog = _scenes;
-        Music = GetNode(_musicPath);
     }
 
     public static void MoveToScene(Node context, int sceneIdx)
     {
-        context.GetTree().ChangeSceneTo(StaticCatalog[sceneIdx]);
+        if (context != null)
+        {
+            context.GetTree().ChangeSceneTo(StaticCatalog[sceneIdx]);
+        }
     }
 }
