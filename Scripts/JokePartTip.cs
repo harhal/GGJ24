8b383ca9ff38b1896cc227059f9237c108aa4d6c
HARHAL-DEV_ICE\harhal <harhal19@gmail.com>
2024-01-28 17:28:22 +0100

    Fades update

--- Scripts/JokePartTip.cs
+++ Scripts/JokePartTip.cs
@@ -4,30 +4,31 @@ using System.Timers;
 
 public class JokePartTip : Node2D
 {
-    private Label Label;
+	private Label Label;
 
-    private float _lifeTime = 0.5f;
-    private float _startTimeMs;
+	private float _lifeTime = 0.5f;
+	private float _startTimeMs;
 
-    public override void _Ready()
-    {
-        Label = GetNode<Label>("Label");
-        _startTimeMs = Time.GetTicksMsec();
-    }
+	public override void _Ready()
+	{
+		Label = GetNode<Label>("Label");
+		_startTimeMs = Time.GetTicksMsec();
+		GetNode<AnimationPlayer>("%FadeOut").Play("1");
+	}
 
-    public void SetText(string SetText, Godot.Color Color, float LifeTime)
-    {
-        Modulate = Color;
-        Label.Text = SetText;
-        _lifeTime = LifeTime;
-    }
+	public void SetText(string SetText, Godot.Color Color, float LifeTime)
+	{
+		Modulate = Color;
+		Label.Text = SetText;
+		_lifeTime = LifeTime;
+	}
 
-    public override void _Process(float delta)
-    {
-        base._Process(delta);
-        if (Time.GetTicksMsec() - _startTimeMs >= _lifeTime * 1000)
-        {
-            QueueFree();
-        }
-    }
+	public override void _Process(float delta)
+	{
+		base._Process(delta);
+		if (Time.GetTicksMsec() - _startTimeMs >= _lifeTime * 1000)
+		{
+			QueueFree();
+		}
+	}
 }
